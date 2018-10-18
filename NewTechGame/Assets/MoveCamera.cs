089ce730cf5605bcbf64c97d448a9f08acccbff7
Atlas <AtlasTheTitan77@gmail.com>
2018-10-18 14:18:54 +0200

    Tobii Eye tracker game commit

---
+++ NewTechGame/Assets/MoveCamera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class MoveCamera : MonoBehaviour {
+	private GazeAware Object;
+	private Vector3 pos;
+	void Start(){
+		Object = GetComponent<GazeAware>();
+	}
+	void Update () {
+		pos = TobiiAPI.GetGazePoint().Screen;
+     	pos.z = 0;
+		Vector2 mousePos = Camera.main.ScreenToViewportPoint(pos);
+		//If player looks left
+		if(mousePos.x <= 0.2f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + ((0.2f / mousePos.x) * -0.1f), transform.localEulerAngles.z);
+		}
+		/*
+		if(mousePos.x <= 0.1f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - 0.2f, transform.localEulerAngles.z);
+		}
+		if(mousePos.x <= 0.05f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - 0.3f, transform.localEulerAngles.z);
+		}
+		*/
+		//Debug.Log("First" + (0.2f / mousePos.x) * -0.15f);
+		//Debug.Log("Second" + (0.2f * (mousePos.x * 10)) * 0.2f);
+
+		//if player looks right
+		if(mousePos.x >= 0.8f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + (0.2f * (mousePos.x * 10)) * 0.2f, transform.localEulerAngles.z);
+		}
+		/*
+		if(mousePos.x >= 0.9f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + 0.2f, transform.localEulerAngles.z);
+		}
+		if(mousePos.x >= 0.95f){
+			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + 0.3f, transform.localEulerAngles.z);
+		}
+		*/
+
+		//If player looks up
+		/* 
+		if(mousePos.y >= 0.9f){
+			if((transform.rotation.x *360) > 100){
+				Debug.Log("it is");
+				transform.eulerAngles = new Vector3(transform.eulerAngles.x + 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
+			}
+			if((transform.rotation.x * 360) <= 100){
+				Debug.Log("eye in the roof");
+				transform.eulerAngles = new Vector3(transform.eulerAngles.x - 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
+			} else {
+				transform.eulerAngles = transform.eulerAngles;
+			}
+		} else {
+			if((transform.rotation.x * 360) >= 1){
+				Debug.Log("eye to the ground");
+				transform.eulerAngles = new Vector3(transform.eulerAngles.x + 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
+			} else {
+				transform.rotation = Quaternion.Euler(Mathf.Clamp(transform.rotation.x, -45,45),0,0);
+			}
+		}
+		*/
+	}
+}
---
+++ NewTechGame/Assets/MovementPlane.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class MovementPlane : MonoBehaviour {
+
+	private GazeAware Object;
+	private Vector3 pos;
+	private Vector3 mousePos;
+	private float step = 0.0f;
+	public GameObject spherePrefab;
+	private GameObject groundSphere;
+
+	public LayerMask ignoreLayer;
+
+	void Start(){
+		Object = GetComponent<GazeAware>();
+	}
+
+	void Update(){
+		moveToPlace();
+		/*
+		if(Input.GetKey(KeyCode.Space)){
+			moveToPlace();
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			if(groundSphere != null){
+				groundSphere.SetActive(false);
+				step = 0;
+			}
+		}
+		*/
+	}
+
+	public void moveToPlace(){
+		pos = TobiiAPI.GetGazePoint().Screen;
+     	pos.z = 0;
+
+		mousePos = Input.mousePosition;
+		 
+		Ray ray = Camera.main.ScreenPointToRay(pos);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, 5, ignoreLayer)) 
+		{	
+			Vector3 placePos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			if(groundSphere == null){
+				groundSphere = Instantiate(spherePrefab, placePos, Quaternion.identity);
+			} else {
+				groundSphere.SetActive(true);
+				groundSphere.transform.position = placePos;
+			}
+			step += 0.01f;
+			groundSphere.transform.GetComponent<Renderer>().material.color = new Color(1,Mathf.Lerp(1,0,step),Mathf.Lerp(1,0,step));	
+			print("eye on floor");
+			if(groundSphere.transform.GetComponent<Renderer>().material.color == Color.red){
+				Camera.main.transform.position = new Vector3(groundSphere.transform.position.x, groundSphere.transform.position.y + 1, groundSphere.transform.position.z);
+				step = 0.0f;
+				groundSphere.SetActive(false);
+			}
+			
+		} else {
+			if(groundSphere != null){
+				groundSphere.SetActive(false);
+			}
+			step = 0.0f;
+		}
+	}
+	public void makeObjectTrackEyes(){
+		Vector3 pos = TobiiAPI.GetGazePoint().Screen;
+     	pos.z = 10;
+     	pos = Camera.main.ScreenToWorldPoint(pos);
+     	transform.position = Vector3.Lerp(transform.position, pos, 10*Time.deltaTime);
+	}
+}
