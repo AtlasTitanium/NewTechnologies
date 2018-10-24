747ab8cab1076311087c777592e1b118ed393374
Atlas <AtlasTheTitan77@gmail.com>
2018-10-24 18:20:44 +0200

    scary people looking at me

---
+++ NewTech/Assets/LookAtPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class LookAtPlayer : MonoBehaviour {
+	private GazeAware Object;
+	private Vector3 pos;
+	private Vector3 mousePos;
+	private float step = 0.0f;
+	private Vector3 lastSeen;
+	private Vector3 newPos;
+
+	void Start(){
+		Object = GetComponent<GazeAware>();
+		lastSeen = Camera.main.transform.position;
+		transform.LookAt(lastSeen);
+	}
+
+	void Update () {
+		pos = TobiiAPI.GetGazePoint().Screen;
+     	pos.z = 0;
+
+		mousePos = Input.mousePosition;
+
+		Ray ray = Camera.main.ScreenPointToRay(mousePos);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 10)) 
+		{
+			if(hit.transform.GetComponent<LookAtPlayer>()){
+				step += 0.05f;
+				float x = (Mathf.Lerp(lastSeen.x, Camera.main.transform.position.x, step));
+				float y = (Mathf.Lerp(lastSeen.y, Camera.main.transform.position.y, step));
+				float z = (Mathf.Lerp(lastSeen.z, Camera.main.transform.position.z, step));
+				newPos = new Vector3(x,y,z);
+				transform.LookAt(newPos);
+				if(newPos == Camera.main.transform.position){
+					lastSeen = Camera.main.transform.position;
+					step = 0;
+				}				
+			} else {
+				lastSeen = newPos;
+				step = 0;
+			}
+		} 
+		
+	}
+}
